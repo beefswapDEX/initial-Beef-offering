df2c0d91f05259e41ac0c73d302dec44c6f92061
Beefswap <98470354+beefswapDEX@users.noreply.github.com>
2022-02-02 23:25:55 +0700

    Create IBOUpgradeProxy.sol

---
+++ contracts/IBOUpgradeProxy.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.6.12;
+
+import "@beefswap/beef-swap-lib/contracts/proxy/TransparentUpgradeableProxy.sol";
+
+contract IBOUpgradeProxy is TransparentUpgradeableProxy {
+
+    constructor(address admin, address logic, bytes memory data) TransparentUpgradeableProxy(logic, admin, data) public {
+
+    }
+
+}
